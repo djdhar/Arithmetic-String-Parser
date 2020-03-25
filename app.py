67b88908f11370ba0346a900b26c1e9567ae4edd
Priti Shaw <34039705+PritiShaw@users.noreply.github.com>
2020-03-25 14:15:03 +0530

    Set permission

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, request, send_from_directory, render_template
 import subprocess,json,os,html
 
 app = Flask(__name__)
+os.system("chmod +x ./processor/a.out")
 
 @app.errorhandler(404)
 def entry_point(e):
     expression = request.args.get("exp")
     if expression is not None:
         expression = bytes(html.unescape(expression), 'utf-8')+b'\n'
-        os.system("chmod +x ./processor/a.out")
         three_address = subprocess.check_output("./processor/a.out", input=expression)
         three_address = str(three_address,'utf-8')
         try:
